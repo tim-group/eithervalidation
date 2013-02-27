05d5e2538d4167fb709eef96379a6c1fac9162ca
Marc Siegel <marc.siegel@timgroup.com>
2013-02-27 10:26:48 -0500

    Documented why Semigroup typeclass is necessary in Scala 2.9
    Hid the typeclass inside an object to avoid polluting namespace,
    and explain clearly that it's due to a Scala bug, and the view
    and context bounds regardling TraversableLike should be sufficient
    in future versions of Scala.

--- src/main/scala/com/youdevise/eithervalidation/EitherValidation.scala
+++ src/main/scala/com/youdevise/eithervalidation/EitherValidation.scala
@@ -47,16 +47,29 @@ import collection.generic.CanBuildFrom
  * becomes natural and discoverable in the normal idioms of Scala. It's about
  * "applying", after all, so why not use #apply.
  *
- * NOTE: This is written to support Scala back to at least 2.9.1. In 2.10 and
- *   beyond, they have simplified some of the implicit typing for the
- *   collections to limit CanBuildFrom's appearance, as explained here:
- *     http://stackoverflow.com/questions/5410846/how-do-i-apply-the-enrich-my-library-pattern-to-scala-collections
+ * Another difference is that we don't publish a Semigroup typeclass to the
+ * top-level namespace. At the moment we, add a private typeclass in order to
+ * support appending to String and Array as well as all TraversableLike
+ * collection classes, but this is only necessary due to SI-3346 [1][2][3][4],
+ * and will likely change in the future -- we'll eliminate the typeclass, and
+ * simply match any type in the left of the either which is viewable as a
+ * TraversableLike without additional ceremony.
+ *
+ * This library is written to support Scala back to at least 2.9.1. In 2.10
+ * and beyond, there have been some simplifications of the implicit typing for
+ * collections to limit the appearance of CanBuildFrom [5].
+ *
+ *   [1] https://issues.scala-lang.org/browse/SI-3346
+ *   [2] http://stackoverflow.com/questions/8472654/how-to-enrich-scala-collections-with-my-own-generic-map-the-right-way/8477943#8477943
+ *   [3] http://www.scala-lang.org/node/11889
+ *   [4] http://yz.mit.edu/wp/true-scala-complexity/
+ *   [5] http://stackoverflow.com/questions/5410846/how-do-i-apply-the-enrich-my-library-pattern-to-scala-collections
  */
 
 /** Wraps an Either which has a TraversableLike type in the left, and a function in the right */
-case class EitherValidation[E : Semigroup, X, Y](either1: Either[E, X => Y]) {
+case class EitherValidation[E : EitherValidation.Semigroup, X, Y](either1: Either[E, X => Y]) {
   def apply(either2: Either[E, X]): Either[E, Y] = {
-    val semigroup = implicitly[Semigroup[E]]
+    val semigroup = implicitly[EitherValidation.Semigroup[E]]
     (either1, either2) match {
       case (Left(e1), Left(e2)) => Left(semigroup.append(e1, e2))
       case (Left(e1), Right(_)) => Left(e1)
@@ -76,23 +89,25 @@ case class EitherWithLeftNothingValidation[X, Y](either1: Either[Nothing, X => Y
   }
 }
 
-trait Semigroup[E] {
-  def append(l: E, r: => E): E
-}
-
-/** Import these implicits into a scope to treat Eithers as validations */
-object EitherValidationImplicits {
-
-  implicit object StringSemigroup extends Semigroup[String] {
-    def append(l: String, r: => String) = l + r
+object EitherValidation {
+  /** A trait to describe anything that can be appended, for the Left of an EitherValidation */
+  trait Semigroup[E] {
+    def append(l: E, r: => E): E
   }
 
-  implicit def eitherWithLeftNothing2eitherWithLeftNothingValidation[E, X, Y](e: Either[Nothing, X => Y]): EitherWithLeftNothingValidation[X, Y] = EitherWithLeftNothingValidation(e)
-  implicit def either2eitherValidation[E : Semigroup, X, Y](e: Either[E, X => Y]): EitherValidation[E, X, Y] = EitherValidation(e)
-
-  implicit def TraversableSemigroup[E1, T[E2] <: TraversableLike[E2, T[E2]]](implicit cbf: CanBuildFrom[T[E1], E1, T[E1]]): Semigroup[T[E1]] = new Semigroup[T[E1]] {
-    override def append(s1: T[E1], s2: => T[E1]): T[E1] = {
-      s1 ++ s2
+  /** Import these implicits into a scope to treat qualified Eithers as EitherValidations */
+  object Implicits {
+    /** We know how to append anything viewable as a TraversableLike */
+    implicit def TraversableLike2EitherValidationSemigroup[E1, T[E2] <: TraversableLike[E2, T[E2]]](implicit cbf: CanBuildFrom[T[E1], E1, T[E1]]): EitherValidation.Semigroup[T[E1]] = new EitherValidation.Semigroup[T[E1]] {
+      override def append(l: T[E1], r: => T[E1]): T[E1] = l ++ r
     }
+
+    /** Only necessary because Scala doesn't realize that String is viewable as TraversableLike, see SI-3346 */
+    implicit object StringEitherValidationSemigroup extends EitherValidation.Semigroup[String] {
+      def append(l: String, r: => String) = l ++ r
+    }
+
+    implicit def EitherWithLeftNothing2EitherWithLeftNothingValidation[E, X, Y](e: Either[Nothing, X => Y]): EitherWithLeftNothingValidation[X, Y] = EitherWithLeftNothingValidation(e)
+    implicit def Either2EitherValidation[E : EitherValidation.Semigroup, X, Y](e: Either[E, X => Y]): EitherValidation[E, X, Y] = EitherValidation(e)
   }
 }
--- src/test/scala/com/youdevise/eithervalidation/EitherValidationSpec.scala
+++ src/test/scala/com/youdevise/eithervalidation/EitherValidationSpec.scala
@@ -2,44 +2,72 @@ package com.youdevise.eithervalidation
 
 import org.specs2.Specification
 
-import com.youdevise.eithervalidation.EitherValidationImplicits._
+import com.youdevise.eithervalidation.EitherValidation.Implicits._
 
 class EitherValidationSpec extends Specification {
   def is =
-    "Examples" ^
-      "Using a curried function and explicit apply method" ^
-        "add(x, y)" ^
-          "should return a right containing sum when both are rights" ! {
-            val f = (add _).curried
-            Right(f).apply(Right(2)).apply(Right(3)) must_== Right(5)
-          } ^
-          "should return a left containing all failures in a list when both are lefts containing lists" ! {
-            val f = (add _).curried
-            Right(f).apply(Left(List("a"))).apply(Left(List("b"))) must_== Left(List("a", "b"))
-          } ^
-          "should return a left containing all failures in a string when both are lefts containing strings" ! {
-            val f = (add _).curried
-            Right(f).apply(Left("a")).apply(Left("b")) must_== Left("ab")
-          } ^
-          bt ^
-        "Person(age, name, postcode)" ^
-          "should return a right containing fields when all validate" ! {
-            val f = (Person.apply _).curried
-            Right(f)(validAge("42"))(validName("Arthur"))(validPostcode("1234")) must_== Right(Person(42, "Arthur", "1234"))
-          } ^
-          "should return a left containing all failures" ! {
-            val f = (Person.apply _).curried
-            Right(f)(validAge("150"))(validName("dude"))(validPostcode("a1")) must_== Left(List("Age must be less than 130", "Name must begin with a capital letter", "Postcode must be 4 digits"))
-          } ^
-          "should return a left containing all failures as a string, when failures are strings" ! {
-            val f = (Person.apply _).curried
-            Right(f)(validAge2("150"))(validName2("dude"))(validPostcode2("a1")) must_== Left("Age must be less than 130\nName must begin with a capital letter\nPostcode must be 4 digits\n")
-          } ^
+    "Using a curried function and explicit apply method" ^
+      "add(x, y)" ^
+        "should return a right containing sum when both are rights" ! {
+          Right(curriedAdd).apply(Right(2)).apply(Right(3)) must_== Right(5)
+        } ^
+        "should return a left containing all failures in a list when both are lefts containing lists" ! {
+          Right(curriedAdd).apply(Left(List("a"))).apply(Left(List("b"))) must_== Left(List("a", "b"))
+        } ^
+        "should return a left containing all failures in a string when both are lefts containing strings" ! {
+          Right(curriedAdd).apply(Left("a")).apply(Left("b")) must_== Left("ab")
+        } ^
+        bt ^
+      "Person(age, name, postcode)" ^
+        "should return a right containing fields when all validate" ! {
+          Right(curriedPersonConstructor).apply(validAge("42")).apply(validName("Arthur")).apply(validPostcode("1234")) must_==
+            Right(Person(42, "Arthur", "1234"))
+        } ^
+        "should return a left containing all failures" ! {
+          Right(curriedPersonConstructor).apply(validAge("150")).apply(validName("dude")).apply(validPostcode("a1")) must_==
+            Left(List("Age must be less than 130", "Name must begin with a capital letter", "Postcode must be 4 digits"))
+        } ^
+        "should return a left containing all failures as a string, when failures are strings" ! {
+          Right(curriedPersonConstructor).apply(validAge2("150")).apply(validName2("dude")).apply(validPostcode2("a1")) must_==
+            Left("Age must be less than 130\nName must begin with a capital letter\nPostcode must be 4 digits\n")
+        } ^
+        bt ^
+      bt ^
+    br ^
+    "Using a curried function and parentheses sugar for apply method" ^
+      "add(x, y)" ^
+        "should return a right containing sum when both are rights" ! {
+          Right(curriedAdd)(Right(2))(Right(3)) must_== Right(5)
+        } ^
+        "should return a left containing all failures in a list when both are lefts containing lists" ! {
+          Right(curriedAdd)(Left(List("a")))(Left(List("b"))) must_== Left(List("a", "b"))
+        } ^
+        "should return a left containing all failures in a string when both are lefts containing strings" ! {
+          Right(curriedAdd)(Left("a"))(Left("b")) must_== Left("ab")
+        } ^
+        bt ^
+      "Person(age, name, postcode)" ^
+        "should return a right containing fields when all validate" ! {
+          Right(curriedPersonConstructor)(validAge("42"))(validName("Arthur"))(validPostcode("1234")) must_==
+            Right(Person(42, "Arthur", "1234"))
+        } ^
+        "should return a left containing all failures" ! {
+          Right(curriedPersonConstructor)(validAge("150"))(validName("dude"))(validPostcode("a1")) must_==
+            Left(List("Age must be less than 130", "Name must begin with a capital letter", "Postcode must be 4 digits"))
+        } ^
+        "should return a left containing all failures as a string, when failures are strings" ! {
+          Right(curriedPersonConstructor)(validAge2("150"))(validName2("dude"))(validPostcode2("a1")) must_==
+            Left("Age must be less than 130\nName must begin with a capital letter\nPostcode must be 4 digits\n")
+        } ^
     end
 
   def add(x: Int, y: Int): Int = x + y
 
+  val curriedAdd = (add _).curried
+
   case class Person(age: Int, name: String, postcode: String)
+
+  val curriedPersonConstructor = (Person.apply _).curried
 
   def validAge(s: String): Either[List[String], Int] =
     try {
@@ -51,7 +79,7 @@ class EitherValidationSpec extends Specification {
       else
         Right(n)
     } catch {
-      case e => Left(List(e.toString))
+      case e: Throwable => Left(List(e.toString))
     }
 
   def validName(s: String): Either[List[String], String] =
@@ -66,29 +94,11 @@ class EitherValidationSpec extends Specification {
     else
       Left(List("Postcode must be 4 digits"))
 
-  def validAge2(s: String): Either[String, Int] =
-    try {
-      val n = s.toInt
-      if (n < 0)
-        Left("Age must be greater than 0\n")
-      else if (n > 130)
-        Left("Age must be less than 130\n")
-      else
-        Right(n)
-    } catch {
-      case e => Left(e.toString)
-    }
+  def mapLeftListOfStringToStringWithNewline[A](e: Either[List[String], A]): Either[String, A] =
+    e.left.map(_.head + "\n")
 
-  def validName2(s: String): Either[String, String] =
-    if (s.headOption exists (_.isUpper))
-      Right(s)
-    else
-      Left("Name must begin with a capital letter\n")
-
-  def validPostcode2(s: String): Either[String, String] =
-    if (s.length == 4 && s.forall(_.isDigit))
-      Right(s)
-    else
-      Left("Postcode must be 4 digits\n")
+  def validAge2(s: String): Either[String, Int] = mapLeftListOfStringToStringWithNewline(validAge(s))
+  def validName2(s: String): Either[String, String] = mapLeftListOfStringToStringWithNewline(validName(s))
+  def validPostcode2(s: String): Either[String, String] = mapLeftListOfStringToStringWithNewline(validPostcode(s))
 }
 
